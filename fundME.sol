9fa5c9ef8c7a4a687ffc1bb117f66bbb42363c65
Suresh Mondal <88233349+Sureshm811@users.noreply.github.com>
2025-12-15 21:40:37 +0530

    Refactor fundMe contract structure and functions

--- fundME.sol
+++ fundME.sol
@@ -3,25 +3,28 @@ pragma solidity 0.8.18;
 import {priceConverter} from "./priceConverter.sol";
 using priceConverter for uint256;
 
-contract fundme {
+contract fundme
+{
     uint256 public minimumUsd = 1e15;
     address[] public funders;
     mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
     
-function fundMe() public payable {
+function fundMe() public payable 
+    {
     require(msg.value.getConversionRate() >= minimumUsd, "You need to spend more ETH!");
     funders.push(msg.sender);
     addressToAmountFunded[msg.sender] = addressToAmountFunded [msg.sender] + msg.value;
-}
-function withdraw () public {
+    }
+function withdraw () public 
+        {
+
     for(uint256 funderIndex = 0;
     funderIndex > funders.length;
     funderIndex++)
     {address funder = funders[funderIndex];
-    addressToAmountFunded[funder]= 0 ;
-    }
+    addressToAmountFunded[funder]= 0 ;}
     
 
-}
+     }
 
 }
