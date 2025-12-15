a2345d8fac72af8b2c48c5f2eb65521bd567daa1
Suresh Mondal <88233349+Sureshm811@users.noreply.github.com>
2025-12-15 21:37:52 +0530

    Correct withdraw function loop for funders
    Fix withdraw function loop condition to iterate correctly.

--- fundME.sol
+++ fundME.sol
@@ -13,6 +13,15 @@ function fundMe() public payable {
     funders.push(msg.sender);
     addressToAmountFunded[msg.sender] = addressToAmountFunded [msg.sender] + msg.value;
 }
+function withdraw () public {
+    for(uint256 funderIndex = 0;
+    funderIndex > funders.length;
+    funderIndex++)
+    {address funder = funders[funderIndex];
+    addressToAmountFunded[funder]= 0 ;
+    }
+    
 
+}
 
 }
